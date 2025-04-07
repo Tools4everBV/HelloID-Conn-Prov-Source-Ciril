27398ae965c7147b8ad74762550e012b7cb27e68
Patrick HORYN <71121140+patrickhoryn@users.noreply.github.com>
2025-04-07 17:58:55 +0200

    Update Persons.ps1

--- Persons.ps1
+++ Persons.ps1
@@ -1,12 +1,11 @@
 # Getting variables from configuration tab
 $config = ConvertFrom-Json $configuration
+$ReferenceDate = $config.ReferenceDate
+
 Try{
-    # Initializing Oracle connexion
-    
-    $config = ConvertFrom-Json $configuration;
-    $count = 0
+    # Initializing Oracle connexion    
     Add-Type -Path $config.DLLPath
- 
+    
     ## connection string ###
     $dataSource = @"
     (DESCRIPTION =
@@ -29,42 +28,37 @@ Try{
     )
 "@    
     # Querying SQL
-    $OracleSQLQuery = "WITH structure_path (st_nostr, st_lib, st_niveau, st_chemin) 
-    AS(
-        SELECT st_nostr, st_lib, 1, st_lib
-        FROM h_struc_pos
-        WHERE st_no_mere is NULL
-    
-        UNION all
-    
-        SELECT h.st_nostr, h.st_lib, sp.st_niveau + 1, sp.st_chemin || '/' || h.st_lib
-        FROM h_struc_pos h
-        JOIN structure_path sp on h.st_no_mere = sp.st_nostr
-    )
-    SELECT distinct DT_APPLI, 
-    DT_NAISSANCE,
-    H_AGT_INI.NOM,
-    H_AGT_INI.PRENOM,
-    substr(NO_MATRICULE,4,8) as NO_MATRICULE,
-    AG_ADRURL as EMAIL_PERSO,
-    DT_FIN_APPLI,
-    (CASE WHEN H_VUE_RESP_HIE.MATRICULE IS NOT NULL THEN H_VUE_RESP_HIE.MATRICULE_RESPONSABLE ELSE NULL END) as MATRICULE_RESPONSABLE,
-    H_VUE_RESP_HIE.POSTE,
-    H_VUE_RESP_HIE.LIBELLE_POSTE,
-    H_VUE_RESP_HIE.CODE_STRUCTURE,
-    H_VUE_RESP_HIE.LIBELLE_STRUCTURE,
-    istruct.st_chemin CHEMIN_STRUCTURE
-    FROM H_AGT_INI
-    LEFT JOIN H_VUE_RESP_HIE ON (concat('001', H_VUE_RESP_HIE.MATRICULE)) = H_AGT_INI.NO_MATRICULE
-    LEFT JOIN structure_path istruct on istruct.st_nostr = H_VUE_RESP_HIE.CODE_STRUCTURE
-    LEFT JOIN H_AGE on (concat('001', H_AGE.ag_matri)) = H_AGT_INI.NO_MATRICULE
-    WHERE (DT_FIN_APPLI  > TO_DATE('01-01-24', 'DD-MM-YY') OR DT_FIN_APPLI IS NULL)
-    AND substr(NO_MATRICULE,4,8) not in (
-        SELECT distinct ag_matri
-        FROM H_AGE
-        WHERE ag_nomusa like '%NPU%'
-    )
+    $OracleSQLQuery ="SELECT 
+        STR_CHEMINLIB,
+        STR_CHEMINCODE,
+        STR_NOSTR,
+        DAA_AA_NUM,
+        DAA_AG_MATRI AS ExternalID,
+        TO_CHAR(DAA_AA_DATDEB, 'MM-DD-YYYY') AS StartDate,
+        TO_CHAR(DAA_AA_DATFIN, 'MM-DD-YYYY') AS EndDate,
+        DAA_AG_TITRE AS Gender,         
+        RTRIM(DAA_AG_NOMTRI) AS FamillyNamePartner,
+        RTRIM(DAA_AG_NOMPAT) AS FamillyName,
+        RTRIM(DAA_AG_NOMUSA),
+        RTRIM(DAA_AG_PRENOM) AS GivenName,
+        RTRIM(AG_SURNOM) AS SecondGivenName,
+        RTRIM(STR_LIB) AS Service,
+        STR_NOSTR As ServiceCode,
+        RTRIM(STR_LIBLON) AS ServiceName,
+        DAA_EP_LIB AS Status,
+        DAA_FI_NO AS TitleCode,
+        RTRIM(DAA_FI_LIB_POS) AS TitleName,
+        STR_RESP_MATRI AS ManagerExternalID
+        FROM GRHPROD_DAA.H_VUE_DAA
+        LEFT JOIN GRHPROD_DAA.H_VUE_DEC_STRUC ON GRHPROD_DAA.H_VUE_DEC_STRUC.STR_NOSTR = GRHPROD_DAA.H_VUE_DAA.DAA_ST_NOSTR
+        LEFT JOIN GRHPROD_DAA.H_VUE_DEC_AGENT ON GRHPROD_DAA.H_VUE_DEC_AGENT.AG_MATRI = GRHPROD_DAA.H_VUE_DAA.DAA_AG_MATRI
+        WHERE
+        --DAA_AG_MATRI = 'XXXXX' AND
+        (DAA_AA_DATFIN  > TO_DATE('$ReferenceDate', 'DD-MM-YYYY') OR DAA_AA_DATFIN IS NULL)
+        AND DAA_AA_NUMORD is not NULL
+        AND RTRIM(DAA_FI_LIB_POS) not like 'VACATAIRE%
     "
+
     ### open up oracle connection to database ###
     $connectionString = "User Id=$($config.username);Password=$($config.password);Data Source=$dataSource"
     $con = [Oracle.ManagedDataAccess.Client.OracleConnection]::new($connectionString)
@@ -79,39 +73,48 @@ Try{
     $SelectDataTable = [System.Data.DataTable]::new()
     $SelectDataTable.Load($cmd.ExecuteReader())
     
-    # Contract Table
-    $Contracts = $SelectDataTable
+     # Contract Table
+    $Contracts = $SelectDataTable | group-object -Property ExternalID -AsHashTable -AsString
+    
     # Persons Table 
-    $persons = $SelectDataTable | sort-object NOM, PRENOM, NO_MATRICULE -unique
+    $persons = $SelectDataTable | sort-object FamillyName, GivenName, ExternalID -unique
 
     foreach($p in $persons){
         $person = @{}
-        $person["ExternalId"] = $p.NO_MATRICULE
-        $person["FirstName"] = $p.PRENOM
-        $person["LastName"] = $p.NOM
-        $person["BirthDate"] = $p.DT_NAISSANCE
-        $person["PersonalEmail"] = $p.EMAIL_PERSO
-        $person["DisplayName"] = "$($p.NOM) $($p.PRENOM) ($($p.NO_MATRICULE))"
+        $person["ExternalId"] = $p.ExternalID
+        $person["Gender"] = $p.Gender 
+        $person["GivenName"] =  if($p.SecondGivenName -notlike ""){$p.SecondGivenName}else{$p.GivenName}
+        $person["FamillyName"] = $p.FamillyName
+        $person["FamillyNamePartner"] = $p.FamillyNamePartner
+        $person["DisplayName"] = "$($p.FamillyNamePartner) $($p.GivenName) ($($p.ExternalID))"
         $person["Contracts"] = [System.Collections.ArrayList]@();
+        
+        foreach ($c in $Contracts["$($p.ExternalID)"]){
+            $contract = @{}
+            $contract["ID"] = $c["ExternalID"] + "-" + $c["DAA_AA_NUM"]
+            $contract["StartDate"] = $c["StartDate"]
+            $contract["EndDate"] = $c["EndDate"]
+            $contract["TitleCode"] = $c["TitleCode"]
+            $contract["TitleName"] = $c["TitleName"]
+            $person["PersTitleName"] = $c["TitleName"]
+            $contract["DGAName"] = $c["STR_CHEMINLIB"].split('\')[4]
+            $contract["DGACode"] = $c["STR_CHEMINCODE"].split('\')[4]
+            $contract["DirectionName"] = $c["STR_CHEMINLIB"].split('\')[5]
+            $contract["DirectionCode"] = $c["STR_CHEMINCODE"].split('\')[5]
+            $contract["ServiceName"] = $c["STR_CHEMINLIB"].split('\')[6]
+            $contract["ServiceCode"] = $c["STR_CHEMINCODE"].split('\')[6]
+            $contract["TeamName"] = $c["STR_CHEMINLIB"].split('\')[7]
+            $contract["TeamCode"] = $c["STR_CHEMINCODE"].split('\')[7]
+            $contract["PoleName"] = $c["STR_CHEMINLIB"].split('\')[8]
+            $contract["PoleCode"] = $c["STR_CHEMINCODE"].split('\')[8]
+            $contract["OrganigrammePath"] = $c["STR_CHEMINLIB"]
+            $contract["ManagerExternalID"] = $c["ManagerExternalID"]
+            $contract["Status"] = $c["Status"]
 
-        foreach ($c in $Contracts.Rows){
-            if ( $c["NO_MATRICULE"] -eq $p.NO_MATRICULE){
-                $contract = @{}
-                $contract["ID"] = $c["NO_MATRICULE"] + $c["POSTE"] + $c["MATRICULE_RESPONSABLE"] + "-" + $c["DT_APPLI"]
-                $contract["StartDate"] = $c["DT_APPLI"]
-                $contract["EndDate"] = $c["DT_FIN_APPLI"]
-                $contract["NumeroPoste"] = $c["POSTE"]
-                $contract["LibellePoste"] = $c["LIBELLE_POSTE"]
-                $contract["CodeStructure"] = $c["CODE_STRUCTURE"]
-                $contract["LibelleStructure"] = $c["LIBELLE_STRUCTURE"]
-                $contract["MatriculeResponsable"] = $c["MATRICULE_RESPONSABLE"]
-                $contract["CheminCompletStructure"] = $c["CHEMIN_STRUCTURE"]
-
-                [void]$person.Contracts.Add($contract)
-            }
+            [void]$person.Contracts.Add($contract)
         }
-    }
-    Write-Output ($person | ConvertTo-Json -Depth 50)
+        Write-Output ($person | ConvertTo-Json -Depth 50)
+    } 
 }
 Catch{
     Write-error "Error when importing persons - $($_.Exception.Message)"
